c164b961f8ab07e9cd8a4fbfb6c5a5b4651f4707
EngySamyy <108956831+EngySamyy@users.noreply.github.com>
2022-11-26 23:30:02 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,14 @@
+// 
+var typed = new Typed (".typing", {
+    strings : ["", "Web Developer","Front End", "Web Design"],
+    typeSpeed : 100,
+    BackSpeed :70,
+    loop:true
+})
+
+// var typed = new Typed(".typing", {
+//     String:["Front_End", "Web_Developer"],
+//     typeSpeed:100,
+//     BackSpeed:60,
+//     loop:true
+// })
